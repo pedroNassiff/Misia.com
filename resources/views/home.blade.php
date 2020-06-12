3ba53b973ae052e5ec107651022637a4c1f06518
Walterisimo <webswalter@gmail.com>
2020-06-11 23:16:04 -0300

    Agregando texto y botón al slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,28 @@
     <div class="row">
       <div class="col-12 content-slider">
         <div class="main-slider">
-          <div><img class="img-fluid" src="{{ asset('assets/images/JPG/slider.jpg') }}" alt="sldier"/></div>
-          <div><img class="img-fluid" src="{{ asset('assets/images/JPG/slider.jpg') }}" alt="sldier"/></div>
+          <div>
+            <img class="img-fluid" src="{{ asset('assets/images/JPG/slider.jpg') }}" alt="sldier"/>
+            <div class="slide-text text-center">
+              <h1 class="mb-4 g-bolditalic">
+                ELEGÍ, CREÁ Y ENCARGÁ TU<br/>VESTIDO DE FIESTA A MEDIDA
+              </h1>
+              <a class="btn h-condensed btn-generic" href="#">
+                COMENZAR A CREAR
+              </a>
+            </div>
+          </div>
+          <div>
+            <img class="img-fluid" src="{{ asset('assets/images/JPG/slider.jpg') }}" alt="sldier"/>
+            <div class="slide-text text-center">
+              <h1 class="mb-4 g-bolditalic">
+                ELEGÍ, CREÁ Y ENCARGÁ TU<br/>VESTIDO DE FIESTA A MEDIDA
+              </h1>
+              <a class="btn h-condensed btn-generic" href="#">
+                COMENZAR A CREAR
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
